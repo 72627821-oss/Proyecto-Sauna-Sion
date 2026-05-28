c7ebafcc5908a55e87bd8240f08f0501f2400d5d
luzitapod <61169530@continental.edu.pe>
2026-05-27 22:27:15 -0500

    Update Proyecto.cpp

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,1 +1,60 @@
-dd# 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*==========================
+   LISTA ENLAZADA
+   REGISTRO DE CLIENTES
+===========================*/
+
+struct Cliente {
+    int id;
+    string nombre;
+    string servicio;
+    int prioridad;
+    Cliente* siguiente;
+};
+
+Cliente* lista = NULL;
+
+// Registrar cliente
+void registrarCliente() {
+
+    Cliente* nuevo = new Cliente();
+
+    cout << "\n=== REGISTRO DE CLIENTE ===\n";
+
+    cout << "Ingrese ID: ";
+    cin >> nuevo->id;
+
+    cin.ignore();
+
+    cout << "Ingrese nombre: ";
+    getline(cin, nuevo->nombre);
+
+    cout << "Ingrese servicio solicitado: ";
+    getline(cin, nuevo->servicio);
+
+    cout << "Ingrese prioridad (1-5): ";
+    cin >> nuevo->prioridad;
+
+    nuevo->siguiente = NULL;
+
+    if(lista == NULL) {
+
+        lista = nuevo;
+
+    } else {
+
+        Cliente* aux = lista;
+
+        while(aux->siguiente != NULL) {
+            aux = aux->siguiente;
+        }
+
+        aux->siguiente = nuevo;
+    }
+
+    cout << "Cliente registrado correctamente.\n";
+}
